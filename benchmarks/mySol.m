2c864da97d8db6a5aac71f8f68d2485c2c1e97c5
Simone Pescuma <simone.pescuma@ensta-paris.fr>
2023-05-31 09:52:37 +0200

    Update

--- benchmarks/mySol.m
+++ benchmarks/mySol.m
@@ -8,17 +8,7 @@ switch TAGbench
         solF = 0*x+1;
         solVx = solDx/(1i*k);
         solVy = solDy/(1i*k);
-%         A = abs(x(1)-x(end));
-%         if A~=0
-%             A = A/abs(A);
-%         end
-%         B = abs(y(1)-y(end));
-%         if B~=0
-%             B = B/abs(B);
-%         end
-%         solDsDs = A * solDx + B * solDy;
     case 'waveguide'
-        L = 1.;
         L = 4.;
         theta = 30.*(pi/180.);
         [solU, solDx, solDy] = waveguide(x,y,k,L,theta);
@@ -33,10 +23,8 @@ switch TAGbench
         solDy = 1i*k*sin(theta) * solU;
         solVx = cos(theta) * solU;
         solVy = sin(theta) * solU;
-%         A = sign(abs(x(1)-x(end)));
-%         B = sign(abs(y(1)-y(end)));
-%         solDsDs = - (k^2) * (cos(theta) * A + sin(theta) * B)^2 * solU;
     otherwise
         warning('Error - No valid benchmark has been set.')
 end
+
 end
