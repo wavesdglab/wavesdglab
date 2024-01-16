f6f03ca8d4519b3a0e5d1eda63e796071cb4f707
Axel Modave <axel.modave@ensta-paris.fr>
2024-01-16 15:06:00 +0100

    update setup

--- setup.m
+++ setup.m
@@ -9,10 +9,17 @@ addpath(genpath('scripts/'));
 addpath(genpath('sources/'));
 addpath(genpath('tools/'));
 
-path1 = getenv('PATH');
-if(~contains(path1, ':/Applications/Gmsh.app/Contents/MacOS/'))
-    path1 = [path1 ':/Applications/Gmsh.app/Contents/MacOS/'];
-    setenv('PATH', path1);
+% directoryGmsh = ':/Applications/Gmsh.app/Contents/MacOS/';
+% directoryGmsh = ':/home/pescuma/Desktop/gmsh-4.11.1-Linux64/bin';
+
+if(exist('directoryGmsh'))
+    path1 = getenv('PATH');
+    if(~contains(path1, directoryGmsh))
+        path1 = [path1 directoryGmsh];
+        setenv('PATH', path1);
+    end
+else
+    error('Variable "directoryGmsh" must be defined in "setup.m".')
 end
 
 set(0, 'DefaultLineLineWidth', 2);
