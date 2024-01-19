2e248179e29247422a2642f16a1caecba49b93c4
Axel Modave <axel.modave@ensta-paris.fr>
2024-01-19 11:12:12 +0100

    cleaning branch Timothee

--- benchmarks/mySol.m
+++ benchmarks/mySol.m
@@ -1,6 +1,6 @@
 function [solU, solDx, solDy, solF, solVx, solVy] = mySol(x,y)
 
-global k TAGbench R_disk L L_PML
+global k TAGbench
 
 switch TAGbench
     case 'cavity'
@@ -24,31 +24,6 @@ switch TAGbench
         solDy = 1i*k*sin(theta) * solU;
         solVx = cos(theta) * solU;
         solVy = sin(theta) * solU;
-    case 'scatteringPML'
-        solU = solScattPlaneWaveHard(k,R_disk,x,y);
-        solF = 0*x;
-        solDx = -1i*k*exp(1i*k*x);
-        solDy = 0*x;
-        solVx = 0*x;
-        solVy = 0*x;
-    case 'scattering_rect'
-        % no solU 
-        theta = 4*pi/10;
-        solU = exp(1i*k*(cos(theta)*x+sin(theta)*y));
-        solF = 0*x;
-        solDx = -1i*k*cos(theta) * exp(1i*k*(cos(theta)*x+sin(theta)*y));
-        solDy = -1i*k*sin(theta) * exp(1i*k*(cos(theta)*x+sin(theta)*y));
-        solVx = 0*x;
-        solVy = 0*x;
-    case 'scattering_square'
-        % no solU 
-        theta = 4*pi/10;
-        solU = exp(1i*k*(cos(theta)*x+sin(theta)*y));
-        solF = 0*x;
-        solDx = -1i*k*cos(theta) * exp(1i*k*(cos(theta)*x+sin(theta)*y));
-        solDy = -1i*k*sin(theta) * exp(1i*k*(cos(theta)*x+sin(theta)*y));
-        solVx = 0*x;
-        solVy = 0*x;
     otherwise
         warning('Error - No valid benchmark has been set.')
 end
