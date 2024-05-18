cdf1dbdd19105634f62b6157503c34f134ae4081
Axel Modave <axel.modave@ensta-paris.fr>
2024-05-18 12:48:21 +0200

    [benchmark] update penetrable disk

--- benchmarks/scattering_disk_penetrable/mySol.m
+++ benchmarks/scattering_disk_penetrable/mySol.m
@@ -3,9 +3,9 @@ function [solU, solDx, solDy, solVx, solVy] = mySol(x,y)
 global omega cAir cObj rhoAir rhoObj Rdisk
 
 solU = scattDiskPenetrable_Solution(omega/cAir,omega/cObj,rhoAir,rhoObj,Rdisk,x,y);
-solDx = 0;
-solDy = 0;
-solVx = 0;
-solVy = 0;
+solDx = zeros(size(x));
+solDy = zeros(size(x));
+solVx = zeros(size(x));
+solVy = zeros(size(x));
 
 end
