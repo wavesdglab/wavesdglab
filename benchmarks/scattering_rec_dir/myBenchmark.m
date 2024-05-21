7d10d12b2c34f3866bf0fe5d494103157d6e4a88
timotheeraynaud <timothee@les-raynaud.fr>
2024-05-21 14:30:34 +0200

    add scatt dir

---
+++ benchmarks/scattering_rec_dir/myBenchmark.m
@@ -0,0 +1,21 @@
+function mesh = myBenchmark()
+
+global h edgTagToBC
+global LdomX LdomY LpmlX LpmlY
+
+if(isempty(LdomX)) LdomX = 1.1; end
+if(isempty(LdomY)) LdomY = 1.1; end
+if(isempty(LpmlX)) LpmlX = 0.1; end
+if(isempty(LpmlY)) LpmlY = 0.1; end
+
+% BCPML, BCObstacle
+edgTag = {1, 2};
+BC = {'DIR0', 'DIR'};
+edgTagToBC = containers.Map(edgTag,BC);
+
+linkMsh = 'benchmarks/scattering_rec_dir/rec_cavity_dir.msh';
+linkGeo = 'benchmarks/scattering_rec_dir/rec_cavity_dir.geo';
+system(['gmsh -2 ' linkGeo ' -v 0 -o ' linkMsh ' -clmax ' num2str(h) ' -clmin ' num2str(h) ' -setnumber LdomX ' num2str(LdomX) ' -setnumber LdomY ' num2str(LdomY) ' -setnumber LpmlX ' num2str(LpmlX) ' -setnumber LpmlY ' num2str(LpmlY)]);
+mesh = readMesh2D(linkMsh);
+
+end
