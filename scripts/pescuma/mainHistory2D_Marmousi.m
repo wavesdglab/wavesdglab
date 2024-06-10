e5bfb5d4fc3848a1f01d4e6cfbd05fa15ff3512b
Simone <simone.pescuma8@hotmail.it>
2024-06-10 17:41:27 +0200

    Update

---
+++ scripts/pescuma/mainHistory2D_Marmousi.m
@@ -0,0 +1,100 @@
+% close all;
+clear all;
+
+global omega edgTagToBC
+global rhoArray cArray etaArray kArray
+global pntSouTag pntSouVal
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% MARMOUSI
+iMax = 1000; iOut = 50; restart = 10;
+benchmark = 'geophysics_marmousi';
+
+degree = 3; freq = 2.5; omega = 2*pi*freq; tol = 1e-100;
+run(benchmark,degree,tol,iMax,iOut,restart);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function run(benchmark,degree,tol,iMax,iOut,restart)
+global omega edgTagToBC nLambda
+global rhoArray cArray etaArray kArray
+global pntSouTag pntSouVal
+
+freq = 2.5;
+omega = 2*pi*freq;
+nLambda = 10/(degree+1);
+PREC = 0;
+
+rho = rhoArray;
+c = cArray;
+eta = etaArray;
+k = kArray;
+
+% Build mesh and DOF manager
+mesh = setupBenchmark2D(benchmark);
+mesh = buildConnectivity2D(mesh);
+dofm = buildDofManager2D_DG(mesh, degree);
+
+% Print coefficients
+writeCoef2D(mesh, cArray, 'output/velocity.pos', "Velocity [m/s]");
+writeCoef2D(mesh, rhoArray, 'output/density.pos', "Density");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp('---------------------------------------------------------');
+disp(['Method CHDG (' benchmark ')']);
+disp('---------------------------------------------------------');
+disp(['    degree              ' num2str(degree)]);
+disp('---------------------------------------------------------');
+
+PREC = 1;
+
+[solA, sysA] = computeSolNum2D_CHDG_heterogeneous_2(mesh, dofm, PREC);
+
+disp('---------------------------------------------------------');
+
+writeField2D(dofm, mesh, solA, 'output/solNum.pos', "solNum");
+system('gmsh output/solNum.pos&');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp('--- Solver CGNR');
+[resRedVecA, resPhyVec1A, error1A] = solverCGNRredu_DG_Marmousi(mesh, dofm, sysA, solA, tol, iMax, iOut, @computeNormError2D_DG_Marmousi);
+
+iterVec = (0:iOut:iMax)';
+
+rezu1A = ["iter" "resRed" "resPhy" "error"];
+rezu2A = [iterVec resRedVecA, resPhyVec1A, error1A];
+name = sprintf('output/historyCGNR_CHDG_0thorder_%s_p%i_omega%g.csv', benchmark, degree, omega);
+writematrix([rezu1A ; rezu2A], name, 'Delimiter', 'semi');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp(['--- Solver GMRES']);
+[resRedVecA, resPhyVec3A, error3A] = solverGMRESredu_DG_Marmousi(mesh, dofm, sysA, solA, tol, iMax, iOut, @computeNormError2D_DG_Marmousi);
+
+iterVec = (0:iOut:iMax)';
+
+rezu1A = ["iter" "resRed" "resPhy" "error"];
+rezu2A = [iterVec resRedVecA, resPhyVec3A, error3A];
+name = sprintf('output/historyGMRES_CHDG_0thorder_%s_p%i_omega%g.csv', benchmark, degree, omega);
+writematrix([rezu1A ; rezu2A], name, 'Delimiter', 'semi');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+figure;
+hold off
+semilogy(iterVec,resPhyVec1A,'-ob','MarkerFaceColor','b','DisplayName','CHDG (0th-order) - CGNR');
+hold on
+semilogy(iterVec,resPhyVec3A,'-ob','MarkerFaceColor','w','DisplayName','CHDG (0th-order) - GMRES');
+% semilogy(iterVec,error1A,'-or','MarkerFaceColor','r','DisplayName','HDG - CGNR');
+% semilogy(iterVec,error3A,'-or','MarkerFaceColor','w','DisplayName','HDG - GMRES');
+box on;
+grid on;
+legend('Location','southwest');
+xlabel('Iteration');
+ylabel('Relative error');
+axis([0 iMax 0.005 1]);
+
+end
