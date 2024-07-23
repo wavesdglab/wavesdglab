2c4147546ad96507eb112e51af1c74705d1410ab
timotheeraynaud <timothee@les-raynaud.fr>
2024-07-23 16:02:29 +0200

    Clean code

--- scripts/raynaud/mainSimu.m
+++ scripts/raynaud/mainSimu.m
@@ -21,7 +21,6 @@ switch benchmark
         tol = 1e-10; maxit = 1000; itout = 50;
     case 'cavity'
         k = 2.01*sqrt(2)*pi;
-%         k = 5.877;
         h = 1/64;
         tol = 1e-10; maxit = 2000; itout =4;
         L = 1;
@@ -70,118 +69,52 @@ disp(['    Dlambda             ' num2str(Dlambda)]);
 disp(['---------------------------------------------------------']);
 
 [~, sysA] = computeSolNum2D(mesh, dofm, PREC);
-eigenvec = computeEigVec2D_cavity(mesh, dofm, 1);
 
-
-% errorL2 = computeNormError2D_CG(mesh, dofm, solA);
-%
-% solP = computeSolProjL2_2D_CG(mesh, dofm);
-% errorProjL2 = computeNormError2D_CG(mesh, dofm, solP);
-% 
-% disp(['    L2-Error (numSol)   ' num2str(errorL2,'%1.2e')]);
-% disp(['    L2-Error (projSol)  ' num2str(errorProjL2,'%1.2e')]);
-% disp(['---------------------------------------------------------']);
-
-% -------------------------------------------------------------------------
-% Write and vizu solution
-% -------------------------------------------------------------------------
-
-% writeField2D(dofm, mesh, solA, 'output/solNum.pos', "solNum");
-% writeField2D(dofm, mesh, solP, 'output/solRef.pos', "solRef");
-% writeField2D(dofm, mesh, solA-solP, 'output/errNum.pos', "errNum");
-% system('gmsh output/solRef.pos output/solNum.pos output/errNum.pos&');
-
-% -------------------------------------------------------------------------
-% Compute eigenvalues/eigenvectors
-% -------------------------------------------------------------------------
-
-% mat = sysA.matPinv * sysA.matA;
-mat = sysA.matP\sysA.matA;
-[eigenvec, eigenval] = eigs(mat,size(mat,1));
-eigenval = diag(eigenval);
-negative_indices = find(eigenval < 0);
-eigV = zeros(size(mat, 1), numel(negative_indices));
-for i = 1:numel(negative_indices)
-    eigV(:, i) = eigenvec(:, negative_indices(i));
-%     writeField2D(dofm,mesh,eigV(:,i),'output/eigv1.pos',"eigv1")
-%     system('gmsh output/eigv1.pos&')
-end
-return;
-% rankEigenVec = rank(eigenvec);
-% condEigenVec = cond(eigenvec);
-% get the largest eigenvalue in absolute value
-% lambdaMax = max(abs(eigenval));
-% get the smallest eigenvalue in absolute value
-% lambdaMin = min(abs(eigenval));
-
-
-% disp(['    Size                ' num2str(size(mat,1))]);
-% disp(['    Rank(eigenvectors)  ' num2str(rankEigenVec)]);
-% disp(['    Cond(eigenvectors)  ' num2str(condEigenVec)]);
-
-% csvwrite(['output/eigA_' num2str(k) '.csv'], eigenval');
-
-% Plot spectrum
-% figure;
-% hold off; scatter(real(eigenval),imag(eigenval));
-% % hold on; plot(fovals(mat,100));
-% grid on; box on;
-% set(0,'DefaultFigureWindowStyle','docked')
-
-% Compute condition number
-% condestMat = condest(mat);
-% 
-% disp(['    Cond(mat)           ' num2str(condestMat,'%1.2e')]);
-% disp(['---------------------------------------------------------']);
-
-% eigtool(full(sysA.matA))
+mat = sysA.matA;
 
 % -------------------------------------------------------------------------
 % Compute iterative solution
 % -------------------------------------------------------------------------
 
-solver = 'GMRES';
-switch solver
-    case 'CGNR'
-        [resRedVec, resPhyVec, errorVec] = solverCGNRredu_CG(mesh, dofm, sysA, tol, maxit, itout, @computeNormError2D_CG);
-    case 'GMRES'
-        [resVec, ~, ~, ~, ~, X] = solverGMRES(mesh, dofm, sysA, tol, maxit, itout, @computeNormError2D_CG);
-end
 
-% it_max = size(resVec,1);
-% bound = zeros(size(resVec,1),1);
-% for i=1:it_max    
-%     bound(i) = 2*((lambdaMax-lambdaMin)/(lambdaMax+lambdaMin))^(floor(i/2));
-% end
+[rrGMRES, ~, ~, ~, ~, X] = solverGMRES_dev(mesh, dofm, sysA, tol, maxit, itout,@computeNormError2D_CG);
+solNum = X(:,end);
 
-return;
 
-solGMRES = X(:,end);
-writeField2D(dofm, mesh, solGMRES, 'output/solGMRES.pos', "solGMRES");
-% writeField2D(dofm, mesh, solP, 'output/solInc.pos', "solInc");
-% writeField2D(dofm, mesh, solGMRES+solP, 'output/solTot.pos', "solTot");
+rrGMRES = [(0:itout:maxit)' rrGMRES];
 
-% get the last non zero residual
-lastNonZero = find(resVec,1,'last');
+errorL2 = computeNormError2D_CG(mesh, dofm, solNum);
+
+solRef = computeSolProjL2_2D_CG(mesh, dofm);
+errorProjL2 = computeNormError2D_CG(mesh, dofm, solRef);
+
+disp(['    L2-Error (numSol)   ' num2str(errorL2,'%1.2e')]);
+disp(['    L2-Error (projSol)  ' num2str(errorProjL2,'%1.2e')]);
+disp(['---------------------------------------------------------']);
+
+% -------------------------------------------------------------------------
+% Write and vizu solution
+% -------------------------------------------------------------------------
+
+writeField2D(dofm, mesh, solNum, 'output/solNum.pos', "solNum");
+writeField2D(dofm, mesh, solRef, 'output/solRef.pos', "solRef");
+writeField2D(dofm, mesh, solNum-solRef, 'output/errNum.pos', "errNum");
+system('gmsh benchmarks/cavity/cavity.msh output/solRef.pos output/solNum.pos output/errNum.pos&');
+
 
 
 figure
 hold on
 set(0,'DefaultFigureWindowStyle','docked')
 
-
-% plot([0 maxit],[errorL2 errorL2],'k--','DisplayName','Relative L2-error (direct)','linewidth', 1);
-% plot([0 maxit],[errorProjL2 errorProjL2],'k:','DisplayName','Relative L2-error (projection)','linewidth', 1);
 semilogy(0:itout:maxit,resVec,'b-o','DisplayName','Relative residual','linewidth', 1,'markersize', 5);
-% semilogy(0:itout:maxit,errorVec,'k-o','DisplayName','Relative L2-error (iterative)','linewidth', 1,'markersize', 5);
 semilogy(0:itout:maxit,bound,'r--','DisplayName','Bound','linewidth', 1);
 set(gca, 'YScale', 'log')
 box on
 grid on
-% xlim([0 lastNonZero+1]);
 xlim([0 maxit]);
 ylim auto;
-title(['CG - ' benchmark ' - ' solver ' - k=' num2str(k) ' - h=' num2str(h) ' - degree=' num2str(degree)'], 'interpreter', 'latex', 'fontsize', 20)
+title(['CG - ' benchmark ' - GMRES - k=' num2str(k) ' - h=' num2str(h) ' - degree=' num2str(degree)'], 'interpreter', 'latex', 'fontsize', 20)
 xlabel('Iteration', 'interpreter', 'Latex', 'fontsize', 15)
 ylabel('Values', 'interpreter', 'Latex', 'fontsize', 15)
 legend('Location', 'southwest', 'fontsize', 15)
