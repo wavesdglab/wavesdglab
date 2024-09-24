b97006f441c85cfa9d8d2030e333ebd763f93c8a
timotheeraynaud <timothee@les-raynaud.fr>
2024-09-24 13:56:06 +0200

    Update benchmark names

--- benchmarks/scattering_openCavity_DIR/myBenchmark.m
+++ benchmarks/scattering_openCavity_DIR/myBenchmark.m
@@ -13,8 +13,8 @@ edgTag = {1, 2};
 BC = {'DIR0', 'DIR'};
 edgTagToBC = containers.Map(edgTag,BC);
 
-linkMsh = 'benchmarks/scattering_rec_dir/rec_cavity_dir.msh';
-linkGeo = 'benchmarks/scattering_rec_dir/rec_cavity_dir.geo';
+linkMsh = 'benchmarks/scattering_openCavity_DIR/scattering_openCavity.msh';
+linkGeo = 'benchmarks/scattering_openCavity_DIR/scattering_openCavity.geo';
 system(['gmsh -2 ' linkGeo ' -v 0 -o ' linkMsh ' -clmax ' num2str(h) ' -clmin ' num2str(h) ' -setnumber LdomX ' num2str(LdomX) ' -setnumber LdomY ' num2str(LdomY) ' -setnumber LpmlX ' num2str(LpmlX) ' -setnumber LpmlY ' num2str(LpmlY)]);
 mesh = readMesh2D(linkMsh);
 
