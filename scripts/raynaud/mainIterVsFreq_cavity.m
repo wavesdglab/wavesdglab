27133c30bd8a854fc70b7fab747e81a2e6ecb872
timotheeraynaud <timothee@les-raynaud.fr>
2025-02-04 13:47:13 +0100

    commit

--- scripts/raynaud/mainIterVsFreq_cavity.m
+++ scripts/raynaud/mainIterVsFreq_cavity.m
@@ -18,7 +18,7 @@ h = 1/64;
 tol = 1e-6; maxit = 2000; itout =4;
 L = 1;
 degree = 1;
-PREC = 0;
+PREC = 1;
 nbEigVec=1;
 
 % Build mesh and DOF manager
@@ -56,12 +56,16 @@ for k = tabfreq
     
     A = sysA.matA;
     b = sysA.rhsA;
+    M = sysA.matP;
     
     if maxit > size(A,2)
         maxit = size(A,2);
     end
 
-    [P,Q] = computeDefOp(nbEigVec, eigvec, A);
+    [Pdef,Qdef,Q] = computeDefOp(nbEigVec, eigvec, A);
+
+    AMinv = A/M;
+
     
     %%%%%%%%%%% No deflation %%%%%%%%%%%
     
@@ -71,21 +75,43 @@ for k = tabfreq
     nbit(tabfreq == k, 2) = itGMRES;
     
     
-    % Compute GMRES with ADEF1 and closest eigvec : (P+Q)*A*x = (P+Q)*b
-    [uAD, ~, ~, itAD, rrAD] = gmres(A*(P+Q),b,[],tol,maxit);
-    itAD = itAD(2);
-    nbit(tabfreq == k, 3) = itAD;
+    % Compute GMRES with right preconditioner : A*Minv*u = b, x = Minv*u
+    [xGMRESPRight, ~, ~, itGMRESPRight, rrGMRESPRight] = gmres(AMinv,b,[],tol,maxit);
+    itGMRESPRight = itGMRESPRight(2);
+    nbit(tabfreq == k, 3) = itGMRESPRight;
     
     
-    % Compute GMRES with DEF1 and closest eigvec : P*A*x = P*b
-    [xD, ~, ~, itD, rrD] = gmres(P*A,P*b,[],tol,maxit);
-    itD = itD(2);
-    nbit(tabfreq == k, 4) = itD;
+    % Compute GMRES with left Def : Pdef*A*u = Pdef*b, x = Q*b + Qdef*u
+    [uDLeft, ~, ~, itDLeft, rrDLeft] = gmres(Pdef*A,Pdef*b,[],tol,maxit);
+    itDLeft = itDLeft(2);
+    nbit(tabfreq == k, 4) = itDLeft;
+
+
+    % Compute GMRES with left Def and right prec : Pdef*AMinv*u = Pdef*b, x = Q*b + Qdef*Minv*u
+    [uDLeftPRight, ~, ~, itDLeftPRight, rrDLeftPRight] = gmres(Pdef*AMinv,Pdef*b,[],tol,maxit);
+    itDLeftPRight = itDLeftPRight(2);
+    nbit(tabfreq == k, 5) = itDLeftPRight;
+
+
+    % Compute GMRES with additive right method : A*(I+Q)*u = b, x = (I+Q)*u
+    [xAddRight, ~, ~, itAddRight, rrAddRight] = gmres(A*(eye(size(A))+Q),b,[],tol,maxit);
+    itAddRight = itAddRight(2);
+    nbit(tabfreq == k, 6) = itAddRight;
+
+
+    % Compute GMRES with additive right method and right preconditioner : A*(Minv+Q)*u = b, x = (Minv+Q)*u
+    [xAddRightPrec, ~, ~, itAddRightPrec, rrAddRightPrec] = gmres(AMinv+A*Q,b,[],tol,maxit);
+    itAddRightPrec = itAddRightPrec(2);
+    nbit(tabfreq == k, 7) = itAddRightPrec;
+
     
     disp(['---------------------------------------------------------']);
     disp(['Number of iterations GMRES: ' num2str(itGMRES)]);
-    disp(['Number of iterations GMRES with ADEF: ' num2str(itAD)]);
-    disp(['Number of iterations GMRES with DEF: ' num2str(itD)]);
+    disp(['Number of iterations GMRES with right preconditioner: ' num2str(itGMRESPRight)]);
+    disp(['Number of iterations GMRES with left deflation: ' num2str(itDLeft)]);
+    disp(['Number of iterations GMRES with left deflation and right preconditioner: ' num2str(itDLeftPRight)]);
+    disp(['Number of iterations GMRES with additive right method: ' num2str(itAddRight)]);
+    disp(['Number of iterations GMRES with additive right method and right preconditioner: ' num2str(itAddRightPrec)]);
     disp(['---------------------------------------------------------']);
     
     
