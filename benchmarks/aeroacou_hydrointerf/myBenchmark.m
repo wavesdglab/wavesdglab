15f9f7791478b7d1eecfdcce6b1f94777f7f1f61
Axel Modave <axel.modave@ensta-paris.fr>
2026-03-30 21:05:10 +0200

    update aeroac

--- benchmarks/aeroacou_hydrointerf/myBenchmark.m
+++ benchmarks/aeroacou_hydrointerf/myBenchmark.m
@@ -7,7 +7,12 @@ BC = {'ABC', 'ABC', 'NEU0', 'NEU0', 'ABC', 'ABC', 'ABC'};
 edgTagToBC = containers.Map(edgTag,BC);
 
 linkMsh = 'output/mesh.msh';
+
+% Mesh with interface
 linkGeo = 'benchmarks/aeroacou_hydrointerf/mesh.geo';
+
+% Mesh without interface
+% linkGeo = 'benchmarks/aeroacou_hydrointerf/mesh2.geo';
 
 system(['gmsh -2 ' linkGeo ' -o ' linkMsh ' -clmax ' num2str(h) ' -clmin ' num2str(h)]);
 mesh = readMesh2D(linkMsh);
